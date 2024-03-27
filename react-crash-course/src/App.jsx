6c624382033d2d1143e12a5796f861e2f1979e46
Udayanga <udayanga.wijesiri@gmail.com>
2024-03-27 12:23:20 +0530

    react-crash-course: implement update job functionality

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -11,6 +11,7 @@ import JobsPage from "./pages/JobsPage"
 import NotFoundPage from "./pages/NotFoundPage"
 import JobPage, { jobLoader } from "./pages/JobPage"
 import AddJobPage from "./pages/AddJobPage"
+import EditJobPage from "./pages/EditJobPage"
 
 const App = () => {
 	const addJob = async (newJob) => {
@@ -33,12 +34,29 @@ const App = () => {
 		return
 	}
 
+	const updateJob = async (updatedJob) => {
+		const res = await fetch(`/api/jobs/${updatedJob.id}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedJob),
+		})
+
+		return
+	}
+
 	const router = createBrowserRouter(
 		createRoutesFromElements(
 			<Route path='/' element={<MainLayout />}>
 				<Route index element={<HomePage />} />
 				<Route path='/jobs' element={<JobsPage />} />
 				<Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+				<Route
+					path='/edit-job/:id'
+					element={<EditJobPage updateJobSubmit={updateJob} />}
+					loader={jobLoader}
+				/>
 				<Route
 					path='/jobs/:id'
 					element={<JobPage deleteJob={deleteJob} />}
