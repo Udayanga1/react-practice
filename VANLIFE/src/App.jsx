a243e781a9746182e0fee618fdf1f7aa08f28a51
Udayanga <udayanga.wijesiri@gmail.com>
2024-03-29 15:07:48 +0530

    VANLIFE: implement vans page

--- VANLIFE/src/App.jsx
+++ VANLIFE/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
 
 function App() {
 	return (
@@ -12,11 +13,13 @@ function App() {
 					</Link>
 					<nav>
 						<Link to='/about'>About</Link>
+						<Link to='/vans'>Vans</Link>
 					</nav>
 				</header>
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/about' element={<About />} />
+					<Route path='/vans' element={<Vans />} />
 				</Routes>
 			</BrowserRouter>
 		</>
