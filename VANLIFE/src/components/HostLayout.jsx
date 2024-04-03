bcbf32342f21984d3096d87aecff4a1fc5aab01b
Udayanga <udayanga.wijesiri@gmail.com>
2024-04-03 12:53:02 +0530

    VANLIFE: change the main navbar to indicate the currently-active route using NavLinks

--- VANLIFE/src/components/HostLayout.jsx
+++ VANLIFE/src/components/HostLayout.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom"
 export const HostLayout = () => {
 	return (
 		<>
-			<nav>
+			<nav className='host-nav'>
 				<Link to='/host'>Dashboard</Link>
 				<Link to='/host/income'>Income</Link>
 				<Link to='/host/reviews'>Reviews</Link>
