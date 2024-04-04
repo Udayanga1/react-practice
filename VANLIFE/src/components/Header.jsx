690a65f3aabbe9a5aebdf1f84dee0687c35b91e1
Udayanga <udayanga.wijesiri@gmail.com>
2024-04-04 07:39:54 +0530

    VANLIFE: change Links to NavLinks in Host navbar to add styling based on active route

--- VANLIFE/src/components/Header.jsx
+++ VANLIFE/src/components/Header.jsx
@@ -1,12 +1,6 @@
 import { Link, NavLink } from "react-router-dom"
 
 const Header = () => {
-	// const activeLinkStyle = {
-	// 	fontWeight: "bold",
-	// 	textDecoration: "underline",
-	// 	color: "#161616",
-	// }
-
 	return (
 		<header>
 			<Link className='site-logo' to='/'>
