093dbd964aa6a19eb140211b1adc55254264fd56
Udayanga <udayanga.wijesiri@gmail.com>
2024-04-29 18:45:26 +0530

    mern-auth: remove project

--- mern-auth/backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-const router = express.Router()
-
-import { authUser } from "../controlllers/userController.js"
-
-router.post("/auth", authUser)
-
-export default router
