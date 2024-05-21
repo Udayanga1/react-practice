9bc7e8b28afbb442ee91d7050aae8f776e40ff74
Udayanga Wijesiri <udayanga.wijesiri@gmail.com>
2024-05-21 06:11:56 +0530

    goals-app: useRef()

--- goals-app/src/components/NewGoal.tsx
+++ goals-app/src/components/NewGoal.tsx
@@ -1,19 +1,25 @@
-import { type FormEvent } from "react"
+import { useRef, type FormEvent } from "react"
 
 const NewGoal = () => {
-	const handleSubmit = (event: FormEvent) => {
+	const goal = useRef<HTMLInputElement>(null)
+	const summary = useRef<HTMLInputElement>(null)
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
+
+		const enteredGoal = goal.current!.value
+		const enteredSummary = summary.current!.value
 	}
 
 	return (
 		<form onSubmit={handleSubmit}>
 			<p>
 				<label htmlFor='goal'>Your goal</label>
-				<input type='text' id='goal' />
+				<input type='text' id='goal' ref={goal} />
 			</p>
 			<p>
 				<label htmlFor='summary'>Short summary</label>
-				<input type='text' id='summary' />
+				<input type='text' id='summary' ref={summary} />
 			</p>
 			<p>
 				<button>Add Goal</button>
