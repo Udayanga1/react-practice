ef1d2bd209697cadb3361cc0bdd4c6a38c3b2c6d
Udayanga Wijesiri <udayanga.wijesiri@gmail.com>
2024-05-22 10:24:44 +0530

    goals-app: building a more dynamic and flexible component

--- goals-app/src/components/CourseGoalList.tsx
+++ goals-app/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import { type ReactNode } from "react"
 import CourseGoal from "./CourseGoal"
 import { type CourseGoal as CGoal } from "../App"
+import InfoBox from "./InfoBox"
 
 type CourseGoalListProps = {
 	goals: CGoal[]
@@ -7,8 +9,23 @@ type CourseGoalListProps = {
 }
 
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+	if (goals.length === 0) {
+		return <InfoBox mode="hint">You have no goals yet. Start adding some!</InfoBox>
+	}
+
+	let warningBox: ReactNode
+
+	if (goals.length >= 4) {
+		warningBox = (
+			<InfoBox mode="warning">
+				Your're collecting a lot of goals. Don't put too much on your plate!
+			</InfoBox>
+		)
+	}
+
 	return (
-		<div>
+		<>
+			{warningBox}
 			<ul>
 				{goals.map((goal) => (
 					<li key={goal.id}>
@@ -18,7 +35,7 @@ const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
 					</li>
 				))}
 			</ul>
-		</div>
+		</>
 	)
 }
 export default CourseGoalList
