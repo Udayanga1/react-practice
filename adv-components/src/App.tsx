9d7af63f5292458740d80286033d89928c0c3c89
Udayanga Wijesiri <udayanga.wijesiri@gmail.com>
2024-06-08 13:22:36 +0530

    adv-components: using forwardRef with ts

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,16 +1,13 @@
-import Button from "./components/Button"
-import Container from "./components/Container"
+import { useRef } from "react"
+
+import Input from "./components/Input"
 
 function App() {
+  const input = useRef<HTMLInputElement>(null)
+
   return (
     <main>
-      <Container as={Button} >Click me</Container>
-      {/* <p>
-        <Button>A Button</Button>
-      </p>
-      <p>
-        <Button href="https://google.com">A Link</Button>
-      </p> */}
+      <Input label="Test" id="test" ref={input}/>
     </main>
   )
 }
