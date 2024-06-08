630889f4f80b023ccd9e2e98ccfe6c698418171d
Udayanga Wijesiri <udayanga.wijesiri@gmail.com>
2024-06-08 16:11:09 +0530

    adv-components: custom Form component

---
+++ adv-components/src/components/Form.tsx
@@ -0,0 +1,10 @@
+import { ComponentPropsWithoutRef } from "react"
+
+type FormProps = ComponentPropsWithoutRef<"form">
+
+const Form = (props: FormProps) => {
+  return (
+    <form {...props}>{props.children}</form>
+  )
+}
+export default Form
