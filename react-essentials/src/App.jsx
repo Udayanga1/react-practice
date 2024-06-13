509336f0b6e0e6b9a4e932e50469d053c170fde4
Udayanga Wijesiri <udayanga.wijesiri@gmail.com>
2024-06-13 09:41:38 +0530

    react-essentials: outputting dynamic values

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -1,11 +1,17 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {description} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
