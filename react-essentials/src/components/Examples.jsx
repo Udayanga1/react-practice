cbf7df449192fcb087c763214e86360b2c742c3b
Udayanga <udayanga.wijesiri@gmail.com>
2024-06-20 11:56:14 +0530

    react-essentials: forwarding props to wrapped elements

--- react-essentials/src/components/Examples.jsx
+++ react-essentials/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { EXAMPLES } from '../data.js';
 import TabButton from "./TabButton";
+import Section from './Section.jsx';
 
 export default function Examples() {
     const [ selectedTopic, setSelectedTopic ] = useState()
@@ -9,13 +10,12 @@ export default function Examples() {
     }
 
     return (
-        <section id="examples">
-          <h2>Examples</h2>
+        <Section title={Examples} id="examples">
           <menu>
           <TabButton isSelected={selectedTopic === 'components' } onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx' } onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton isSelected={selectedTopic === 'props' } onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === 'state' } onSelect={() => handleSelect('state')}>State</TabButton>
+          <TabButton isSelected={selectedTopic === 'jsx' } onClick={() => handleSelect('jsx')}>JSX</TabButton>
+          <TabButton isSelected={selectedTopic === 'props' } onClick={() => handleSelect('props')}>Props</TabButton>
+          <TabButton isSelected={selectedTopic === 'state' } onClick={() => handleSelect('state')}>State</TabButton>
           </menu>
           {!selectedTopic ? <p>Please select a topic</p> : <div id="tab-component">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
@@ -26,8 +26,6 @@ export default function Examples() {
               </code>
             </pre>
           </div>}
-            
-          
-        </section>
+        </Section>
     )
 }
