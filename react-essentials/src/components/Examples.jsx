c7f057fde1a69f75b51e931c2b76b65ebd905643
Udayanga <udayanga.wijesiri@gmail.com>
2024-06-26 08:46:12 +0530

    react-essentials: working with multiple jsx slots

--- react-essentials/src/components/Examples.jsx
+++ react-essentials/src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { EXAMPLES } from '../data.js';
 import TabButton from "./TabButton";
 import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 
 export default function Examples() {
     const [ selectedTopic, setSelectedTopic ] = useState()
@@ -11,21 +12,24 @@ export default function Examples() {
 
     return (
         <Section title={Examples} id="examples">
-          <menu>
-          <TabButton isSelected={selectedTopic === 'components' } onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx' } onClick={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton isSelected={selectedTopic === 'props' } onClick={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === 'state' } onClick={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          {!selectedTopic ? <p>Please select a topic</p> : <div id="tab-component">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-              {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>}
+          <Tabs buttons={
+            <>
+              <TabButton isSelected={selectedTopic === 'components' } onSelect={() => handleSelect('components')}>Components</TabButton>
+              <TabButton isSelected={selectedTopic === 'jsx' } onClick={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton isSelected={selectedTopic === 'props' } onClick={() => handleSelect('props')}>Props</TabButton>
+              <TabButton isSelected={selectedTopic === 'state' } onClick={() => handleSelect('state')}>State</TabButton>
+              </>
+          }>
+            {!selectedTopic ? <p>Please select a topic</p> : <div id="tab-component">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>}
+          </Tabs>          
         </Section>
     )
 }
