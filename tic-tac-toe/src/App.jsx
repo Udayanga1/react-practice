4a6268340d4b6558fe51af92d1993dafe34b1caa
Udayanga <udayanga.wijesiri@gmail.com>
2024-08-02 10:26:03 +0530

    tic-tac-toe: splitting components and building reusable player component

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,9 +1,19 @@
+import Player from "./components/Player"
 
 function App() {
   
 
   return (
-    <p>Coming soon</p>
+    <main>
+      <div id="game-container">
+        <div id="players">
+          <Player name="Player 1" symbol="X" />  
+          <Player name="Player 2" symbol="O" />  
+        </div>
+
+        GAME BOARD
+      </div>
+    </main>
   )
 }
 
