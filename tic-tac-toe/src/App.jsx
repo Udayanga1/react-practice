bd61ac0f58d6920baf62c703e97095e0138061ff
Udayanga <udayanga.wijesiri@gmail.com>
2024-08-12 10:24:04 +0530

    tic-tac-toe: lifting state up

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
 
   return (
     <main>
       <div id="game-container">
-        <div id="players">
-          <Player initialName="Player 1" symbol="X" />  
-          <Player initialName="Player 2" symbol="O" />  
+        <div id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>  
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />  
         </div>
 
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   )
