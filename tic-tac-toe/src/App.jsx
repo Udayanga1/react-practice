aa9285613fb29af53bd0415af6b9a03e8e621e5b
Udayanga <udayanga.wijesiri@gmail.com>
2024-10-21 10:43:32 +0530

    tic-tac-toe: why immutability matters - always

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for(const turn of gameTurns) {
     const { square, player } = turn; 
@@ -60,6 +60,10 @@ function App() {
     });
   }
 
+  function handleRestart(){
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -68,7 +72,7 @@ function App() {
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />  
         </div>
 
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard 
           onSelectSquare={handleSelectSquare}
           board={gameBoard}
